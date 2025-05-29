d35c5dd6f078db6601d6ff428d75f7c1d1753415
Melika <89937665+melikazn@users.noreply.github.com>
2025-05-29 19:55:29 +0330

    Update UserVocabularyOverviewPage.tsx

--- src/pages/user/UserVocabularyOverviewPage.tsx
+++ src/pages/user/UserVocabularyOverviewPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../api/axios";
 import SearchInput from "../../components/General/SearchInput";
