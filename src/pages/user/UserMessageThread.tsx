5068fbcef30fef0e395fb6eeb2eb6543a7518899
Melika <89937665+melikazn@users.noreply.github.com>
2025-05-29 19:35:08 +0330

    Update UserMessageThread.tsx

--- src/pages/user/UserMessageThread.tsx
+++ src/pages/user/UserMessageThread.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
 import api from "../../api/axios";
 import MessageReplyThread from "../../components/Messages/MessageReplyThread";
 import ReplyInput from "../../components/Messages/ReplyInput";
