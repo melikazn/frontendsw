295b0c1acd0f03aa9078bde6df8862f309347ca9
Melika <89937665+melikazn@users.noreply.github.com>
2025-05-30 20:23:50 +0330

    Update UserNotificationDetail.tsx

--- src/pages/user/UserNotificationDetail.tsx
+++ src/pages/user/UserNotificationDetail.tsx
@@ -1,4 +1,3 @@
-// src/pages/user/UserNotificationDetail.tsx
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "../../api/axios";
